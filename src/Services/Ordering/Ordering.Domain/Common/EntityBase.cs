8b4997fbf9959c6b15e5d50ee21cfaf271a08c17
thomasfregene <fregzythomas@gmail.com>
2024-01-12 19:33:51 +0100

    developed and containerized order service

---
+++ src/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Common
+{
+    public abstract class EntityBase
+    {
+        public int Id { get; set; }
+        public string CraetedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+using Ordering.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+    {
+        public OrderRepository(OrderContext orderContext) :base(orderContext)
+        {
+
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUsername(string username)
+        {
+            var orderList = await _orderContext.Orders
+                .Where(o => o.UserName == username)
+                .ToListAsync();
+
+            return orderList;
+        }
+    }
+}
